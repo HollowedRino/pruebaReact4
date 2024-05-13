6b9ebb59284e9c8aec5c48ada4bd10da1e77de75
H04327 <H04327@lab.aca>
2024-05-13 16:25:48 -0500

    tercer commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,33 @@
 import './App.css';
 import FormIngreso from './components/FormIngreso';
 import ListadoComponentes from './components/ListadoComponentes';
-
+import { useState } from 'react';
 
 function App() {
-  let tem = [1,2,3,4,5];
+  let tem = [];
+  const [listaPersonas, setListaPersonas] = useState([]);
 
-  function solicitarRandoms(){
+  async function solicitarRandoms(){
     console.log("Solicita random");
-    //http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=5
-    let url_base = "http://www.randomnumberapi.com/api/v1.0/random";
-    let numMin = document.getElementById("numMin").value;
-    let numMax = document.getElementById("numMax").value;
+    //https://randomuser.me/api/?results=10
+    let url_base = "https://randomuser.me/api/";
     let cantRandom = document.getElementById("cantRandom").value;
-    let url = url_base + "?min=" + numMin + "&max=" + numMax + "&count=" + cantRandom;
-    console.log(url)
-    fetch(url)
-    .then(function(response){
-      if(!response.ok){ // status 200
-        throw Error("Ocurrio un error: " + response.statusText);
-      }
-    })
-    .then(function(response){
-      return response.json(); // response.text();
-    })
-    .then(function(data){
-      console.log(data);
-    })
-    .catch(function(err){
-      console.log("Ocurrio un error: " + err);
-    });
-    //fetch(url);
+    let url = url_base + "?results=" + cantRandom;
+    console.log(url);
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    setListaPersonas([
+      ...data.results
+    ]);
   }
 
   return (
     <>
       <h1>Uso de API para generar randoms</h1>
       <FormIngreso handlerClick={solicitarRandoms}/>
-      <ListadoComponentes numeros={tem}/>
+      <ListadoComponentes personas={listaPersonas}/>
     </>
     
   );
--- src/components/FormIngreso.js
+++ src/components/FormIngreso.js
@@ -1,12 +1,6 @@
 function FormIngreso({handlerClick}){
     return(
         <>
-            <label for="numMin">Valor minimo: </label>
-            <input type="number" id="numMin" name="numMin"></input>
-            <br/>
-            <label for="numMax">Valor maximo: </label>
-            <input type="number" id="numMax" name="numMax"></input>
-            <br/>
             <label for="cantRandom">Cantidad: </label>
             <input type="number" id="cantRandom" name="cantRandom"></input>
             <br/>
